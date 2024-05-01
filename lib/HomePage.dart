188db557a82f1cd1e65d3d0670e49f022ae7ab8c
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-05-01 16:33:24 +0530

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your Groups',
+      home: GroupsScreen(),
+    );
+  }
+}
+
+class GroupsScreen extends StatefulWidget {
+  @override
+  _GroupsScreenState createState() => _GroupsScreenState();
+}
+
+class _GroupsScreenState extends State<GroupsScreen> {
+  final List<String> groups = [
+    'MPG Activities',
+    'Sant (Su)perman Activities',
+    'Sant(Su)perman Activities (MPG)',
+    'Satsang',
+    'Youth Association Activities',
+  ];
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Groups'),
+        backgroundColor: Colors.green,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              // Open the menu drawer
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text(
+                "John Doe",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              accountEmail: Text(""),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "JD",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+                  color: _selectedIndex == 0 ? Colors.green : null),
+              title: Text(
+                'Home',
+                style: TextStyle(
+                    color: _selectedIndex == 0 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 0;
+                });
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person,
+                  color: _selectedIndex == 1 ? Colors.green : null),
+              title: Text(
+                'Member List',
+                style: TextStyle(
+                    color: _selectedIndex == 1 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 1;
+                });
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list_alt,
+                  color: _selectedIndex == 2 ? Colors.green : null),
+              title: Text(
+                'Local Attendance',
+                style: TextStyle(
+                    color: _selectedIndex == 2 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 2;
+                });
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list_alt,
+                  color: _selectedIndex == 3 ? Colors.green : null),
+              title: Text(
+                "Today's Attendance Split",
+                style: TextStyle(
+                    color: _selectedIndex == 3 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 3;
+                });
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.group,
+                  color: _selectedIndex == 4 ? Colors.green : null),
+              title: Text(
+                'Visitor Management',
+                style: TextStyle(
+                    color: _selectedIndex == 4 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 4;
+                });
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.web,
+                  color: _selectedIndex == 5 ? Colors.green : null),
+              title: Text(
+                'Hazari Web',
+                style: TextStyle(
+                    color: _selectedIndex == 5 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 5;
+                });
+                // Add navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                  color: _selectedIndex == 6 ? Colors.green : null),
+              title: Text(
+                'Settings',
+                style: TextStyle(
+                    color: _selectedIndex == 6 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 6;
+                });
+                // Add navigation logic here
+              },
+            ),
+            // Add a Divider after Settings
+            Divider(
+              color: Colors.grey,
+            ),
+            // Add Assistance title
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Text(
+                'Assistance',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // Add Tutorials list item
+            ListTile(
+              leading: Icon(Icons.menu_book,
+                  color: _selectedIndex == 7 ? Colors.green : null),
+              title: Text(
+                'Tutorials',
+                style: TextStyle(
+                    color: _selectedIndex == 7 ? Colors.green : null,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 7;
+                });
+                // Add navigation logic here
+              },
+            ),
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: groups.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(groups[index]),
+            trailing: Icon(Icons.download),
+          );
+        },
+      ),
+    );
+  }
+}
