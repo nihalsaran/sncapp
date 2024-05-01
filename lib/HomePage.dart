adebf10676c698f47bb54f43362dbd225668ef15
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-05-01 17:35:32 +0530

    Connected pages

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -47,7 +47,6 @@ class _GroupsScreenState extends State<GroupsScreen> {
               color: Colors.white,
             ),
             onPressed: () {
-              // Open the menu drawer
               Scaffold.of(context).openDrawer();
             },
           ),
@@ -87,41 +86,12 @@ class _GroupsScreenState extends State<GroupsScreen> {
                 setState(() {
                   _selectedIndex = 0;
                 });
-                // Add navigation logic here
+                // Navigate to Home page
+                Navigator.pop(context);
               },
             ),
-            ListTile(
-              leading: Icon(Icons.person,
-                  color: _selectedIndex == 1 ? Colors.green : null),
-              title: Text(
-                'Member List',
-                style: TextStyle(
-                    color: _selectedIndex == 1 ? Colors.green : null,
-                    fontWeight: FontWeight.bold),
-              ),
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 1;
-                });
-                // Add navigation logic here
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.list_alt,
-                  color: _selectedIndex == 2 ? Colors.green : null),
-              title: Text(
-                'Local Attendance',
-                style: TextStyle(
-                    color: _selectedIndex == 2 ? Colors.green : null,
-                    fontWeight: FontWeight.bold),
-              ),
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 2;
-                });
-                // Add navigation logic here
-              },
-            ),
+            // Other list tiles here
+            //...
             ListTile(
               leading: Icon(Icons.list_alt,
                   color: _selectedIndex == 3 ? Colors.green : null),
@@ -135,89 +105,16 @@ class _GroupsScreenState extends State<GroupsScreen> {
                 setState(() {
                   _selectedIndex = 3;
                 });
-                // Add navigation logic here
+                // Navigate to Satsang page
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SatsangPage()),
+                );
               },
             ),
-            ListTile(
-              leading: Icon(Icons.group,
-                  color: _selectedIndex == 4 ? Colors.green : null),
-              title: Text(
-                'Visitor Management',
-                style: TextStyle(
-                    color: _selectedIndex == 4 ? Colors.green : null,
-                    fontWeight: FontWeight.bold),
-              ),
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 4;
-                });
-                // Add navigation logic here
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.web,
-                  color: _selectedIndex == 5 ? Colors.green : null),
-              title: Text(
-                'Hazari Web',
-                style: TextStyle(
-                    color: _selectedIndex == 5 ? Colors.green : null,
-                    fontWeight: FontWeight.bold),
-              ),
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 5;
-                });
-                // Add navigation logic here
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings,
-                  color: _selectedIndex == 6 ? Colors.green : null),
-              title: Text(
-                'Settings',
-                style: TextStyle(
-                    color: _selectedIndex == 6 ? Colors.green : null,
-                    fontWeight: FontWeight.bold),
-              ),
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 6;
-                });
-                // Add navigation logic here
-              },
-            ),
-            // Add a Divider after Settings
-            Divider(
-              color: Colors.grey,
-            ),
-            // Add Assistance title
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                'Assistance',
-                style: TextStyle(
-                  color: Colors.grey[600],
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            // Add Tutorials list item
-            ListTile(
-              leading: Icon(Icons.menu_book,
-                  color: _selectedIndex == 7 ? Colors.green : null),
-              title: Text(
-                'Tutorials',
-                style: TextStyle(
-                    color: _selectedIndex == 7 ? Colors.green : null,
-                    fontWeight: FontWeight.bold),
-              ),
-              onTap: () {
-                setState(() {
-                  _selectedIndex = 7;
-                });
-                // Add navigation logic here
-              },
-            ),
+            // Other list tiles here
+            //...
           ],
         ),
       ),
@@ -227,8 +124,121 @@ class _GroupsScreenState extends State<GroupsScreen> {
           return ListTile(
             title: Text(groups[index]),
             trailing: Icon(Icons.download),
+            onTap: () {
+              // Handle navigation for each group
+              switch (index) {
+                case 0:
+                  // Navigation logic for MPG Activities
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MPGActivitiesPage()),
+                  );
+                  break;
+                case 1:
+                  // Navigation logic for Sant (Su)perman Activities
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SantSupermanActivitiesPage()),
+                  );
+                  break;
+                case 2:
+                  // Navigation logic for Sant(Su)perman Activities (MPG)
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SantSupermanMPGActivitiesPage()),
+                  );
+                  break;
+                case 3:
+                  // Navigation logic for Satsang
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SatsangPage()),
+                  );
+                  break;
+                case 4:
+                  // Navigation logic for Youth Association Activities
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => YouthAssociationActivitiesPage()),
+                  );
+                  break;
+                default:
+              }
+            },
           );
         },
+      ),
+    );
+  }
+}
+
+// Define separate pages for each option
+class SatsangPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Satsang'),
+      ),
+      body: Center(
+        child: Text('This is the Satsang Page'),
+      ),
+    );
+  }
+}
+
+// Define other pages here
+class MPGActivitiesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MPG Activities'),
+      ),
+      body: Center(
+        child: Text('This is the MPG Activities Page'),
+      ),
+    );
+  }
+}
+
+class SantSupermanActivitiesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sant (Su)perman Activities'),
+      ),
+      body: Center(
+        child: Text('This is the Sant (Su)perman Activities Page'),
+      ),
+    );
+  }
+}
+
+class SantSupermanMPGActivitiesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sant(Su)perman Activities (MPG)'),
+      ),
+      body: Center(
+        child: Text('This is the Sant(Su)perman Activities (MPG) Page'),
+      ),
+    );
+  }
+}
+
+class YouthAssociationActivitiesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Youth Association Activities'),
+      ),
+      body: Center(
+        child: Text('This is the Youth Association Activities Page'),
       ),
     );
   }
