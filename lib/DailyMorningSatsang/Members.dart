c8b06c28dcf059d535a668b3e6c119f06419e828
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-05-03 11:40:21 +0530

    Member list in satsang updated

--- lib/DailyMorningSatsang/Members.dart
+++ lib/DailyMorningSatsang/Members.dart
@@ -1,11 +1,102 @@
-// page2.dart
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class Page2 extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Page 2 Content'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Page2(),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatefulWidget {
+  @override
+  _Page2State createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  List<Map<String, dynamic>> users = [];
+  Map<String, bool> checkboxValues = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCurrentUserLocation();
+  }
+
+
+  Future<void> fetchCurrentUserLocation() async {
+    // Get current user
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      // Retrieve user document from Firestore
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      // Get user's location
+      String? currentUserLocation = userDoc['location'];
+
+      if (currentUserLocation != null) {
+        // Fetch users' data based on the user's location
+        QuerySnapshot branchDataSnapshot = await FirebaseFirestore.instance
+            .collection('BranchData')
+            .doc(currentUserLocation)
+            .collection('Satsangis')
+            .get();
+
+        // Update the list with data from Satsangis collection
+        setState(() {
+          users = branchDataSnapshot.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data() as Map<String, dynamic>;
+            return {
+              'firstName': data['firstName'] ?? '',
+              'middleName': data['middleName'] ?? '',
+              'lastName': data['lastName'] ?? '',
+              'id': data['id'] ?? '',
+              'location': data['location'] ?? '',
+            };
+          }).toList();
+        });
+      }
+    }
+  }
+
+ @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        final user = users[index];
+        final userId = user['id'];
+        return ListTile(
+          leading: CircleAvatar(
+            child: Text('${user['id']}'),
+          ),
+          title: Text(
+            '${user['firstName']} ${user['middleName']} ${user['lastName']}',
+          ),
+          subtitle: Text(user['location']),
+          trailing: Checkbox(
+            value: checkboxValues[userId] ?? false,
+            onChanged: (value) {
+              setState(() {
+                checkboxValues[userId] = value ?? false;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
